d15f4688aa707665dbb190747b4adf6111b56c2a
abhishek 1199 <abhishekshaw1199@gmail.com>
2024-09-20 17:59:57 +0530

    nav

--- house-rent/src/App.js
+++ house-rent/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+
+import {ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Explore from "./pages/Explore";
+import Offer from "./pages/Offer";
+import Category from "./pages/Category";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import CreateListing from "./pages/CreateListing";
+import EditListing from "./pages/EditListing";
+import Listing from "./pages/Listing";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Navbar/>
+      
+
+      <ToastContainer/>
     </div>
   );
 }
