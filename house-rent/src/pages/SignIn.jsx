ffac902ec3d89dfca49e87851a5464adc77eb2a6
abhishek 1199 <abhishekshaw1199@gmail.com>
2024-09-21 21:37:40 +0530

    fiberresolve

--- house-rent/src/pages/SignIn.jsx
+++ house-rent/src/pages/SignIn.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { useState } from "react";
 import { toast } from "react-toastify";
 import { Link, useNavigate } from "react-router-dom";
-import { getAuth, signInWithEmailAndPassword } from "firebase";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
